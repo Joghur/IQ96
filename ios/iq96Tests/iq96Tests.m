41f432601791f0e2b639e5aa4b621da0fde712a6
martin <joghur@gmail.com>
2021-10-03 10:58:13 +0200

    renamed project

--- ios/iq96Tests/iq96Tests.m
+++ ios/iq96Tests/iq96Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FirebaseAuthAppTests : XCTestCase
+@interface iq96Tests : XCTestCase
 
 @end
 
-@implementation FirebaseAuthAppTests
+@implementation iq96Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
